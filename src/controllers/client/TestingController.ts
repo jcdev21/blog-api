18a064d60ad1edfe9e06097184bb6a0fc1cd2dac
jcdev21 <jcdev21@gmail.com>
2020-08-19 06:01:38 -0400

    okeeee

--- src/controllers/client/TestingController.ts
+++ src/controllers/client/TestingController.ts
@@ -1,12 +1,15 @@
 import { Request, Response } from 'express';
+import ClientBlogService from '../../services/client/ClientBlogService';
 
 class TestingController {
     index = async (req: Request, res: Response): Promise<Response> => {
+        const service = new ClientBlogService(req);
+        const blogs = await service.getAll();
+
         return res.send({
-            data: [{
-                status: 'oke'
-            }],
-            msg: 'success'
+            data: blogs.rows,
+            totalResults: blogs.count,
+            msg: ''
         });
     }
 }
